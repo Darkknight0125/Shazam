505330a790ae563b90bab702cf736d510b131ab7
Darkknight0125 <samansidhu0125@gmail.com>
2025-06-23 23:12:13 +0530

    Migrated tmdb api to backend

--- src/redux/services/tmdbApi.js
+++ src/redux/services/tmdbApi.js
@@ -1,41 +1,68 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const TMDB_API_KEY = '9101c8f4d4339e70cd8836997e7fd98a'; // Replace with your TMDB API key
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+import { BASE_API_URL } from '../../constants/api';
 
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: TMDB_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_API_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getTopMovies: builder.query({
-      query: () => `/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+      query: () => '/api/media/movies/popular',
+      transformResponse: (response) => response,
+      providesTags: ['TopMovies'],
     }),
     getTopSeries: builder.query({
-      query: () => `/tv/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+      query: () => '/api/media/series/popular',
+      transformResponse: (response) => response,
+      providesTags: ['TopSeries'],
     }),
     getTopMoviesByGenre: builder.query({
-      query: ({ genreId }) => `/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${genreId}&language=en-US&page=1`,
+      query: ({ genreId }) => `/api/media/movies/genre/${genreId}`,
+      transformResponse: (response) => response,
+      providesTags: (result, error, { genreId }) => [{ type: 'MoviesByGenre', id: genreId }],
     }),
     getMovieDetails: builder.query({
-      query: ({ movieId }) => `/movie/${movieId}?api_key=${TMDB_API_KEY}&language=en-US&append_to_response=credits,images,videos`,
+      query: ({ movieId }) => `/api/media/movies/${movieId}`,
+      transformResponse: (response) => response,
+      providesTags: (result, error, { movieId }) => [{ type: 'MovieDetails', id: movieId }],
     }),
     getSeriesDetails: builder.query({
-      query: ({ seriesId }) => `/tv/${seriesId}?api_key=${TMDB_API_KEY}&language=en-US&append_to_response=credits,images,videos`,
+      query: ({ seriesId }) => `/api/media/series/${seriesId}`,
+      transformResponse: (response) => response,
+      providesTags: (result, error, { seriesId }) => [{ type: 'SeriesDetails', id: seriesId }],
     }),
     getMovieImg: builder.query({
-      query: ({ movieId }) => `/movie/${movieId}/images?api_key=${TMDB_API_KEY}`,
+      query: ({ movieId }) => `/api/media/movies/${movieId}/images`,
+      transformResponse: (response) => response,
+      providesTags: (result, error, { movieId }) => [{ type: 'MovieImages', id: movieId }],
     }),
     getComingSoon: builder.query({
-      query: () => `/movie/upcoming?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+      query: () => '/api/media/movies/upcoming',
+      transformResponse: (response) => response,
+      providesTags: ['ComingSoon'],
     }),
     getFindMovie: builder.query({
-      query: ({ search }) => `/search/multi?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(search)}&language=en-US&page=1`,
+      query: ({ search }) => `/api/media/search/multi?query=${encodeURIComponent(search)}`,
+      transformResponse: (response) => response,
+      providesTags: (result, error, { search }) => [{ type: 'Search', id: search }],
     }),
     getGenres: builder.query({
-      query: () => `/genre/movie/list?api_key=${TMDB_API_KEY}&language=en-US`,
+      query: () => '/api/media/genres',
+      transformResponse: (response) => response,
+      providesTags: ['Genres'],
     }),
     getTrendingMovies: builder.query({
-      query: () => `/trending/movie/week?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+      query: () => '/api/media/trending/movies',
+      transformResponse: (response) => response,
+      providesTags: ['TrendingMovies'],
     }),
   }),
 });
